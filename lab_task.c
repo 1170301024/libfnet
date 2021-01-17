f400ae52dcb9b32d75855019d580f85f0abaefaf
yzsong <cloudzu@cloudzu.ubuntu>
2021-01-17 14:19:49 +0800

    fixes some bugs

--- lab_task.c
+++ lab_task.c
@@ -1,8 +1,9 @@
 #include    <stdio.h>
 #include    <string.h>
 #include    <stdlib.h>
+#define __USE_XOPEN     // for use strptime function
 #include    <time.h>
-
+#undef __USE_XOPEN
 #include    "include/connect_manage.h"
 #include    "include/dispatch.h"
 #include    "include/error.h"
@@ -10,21 +11,22 @@
 #include    "include/fnetlib.h"
 #include    "debug.h"
 
-#define start_string "#start"
-#define end_string "#end"
 
 
-
+// for packets feature in a flow record
 struct packets{
     int no_pkt;
     struct packet_info *packets;
 };
+
+// denote each packet in a flow record
 struct packet_info{
     int size;
     int dir; /*  0 for inbound. 1 for outbound*/
     int ms_time;
 };
 
+// denote each action in logfile
 struct action{
     time_t start_time;
     time_t end_time;
@@ -33,19 +35,23 @@ struct action{
 };
 
 struct action *action_list;
+
 /*
- * {"b":size,"dir":"<","ipt":ipt}
+ * {"b":pkt_len,"dir":"<","ipt":ipt}
+ * {"rep":65536-pkt_len,"dir":"<", "ipt":ipt}
  */
-int 
-get_packet_size_dir_time(const char *str, struct packet_info * pi){
+static int 
+get_packet_info(const char *str, struct packet_info * pi){
 
     int size_offset = 5, dir_offset, time_offset;
     char num_str[256];
     if(fnet_atoi(&pi->size, str + size_offset) == -1){
-        puts(str);
-        err_msg("error");
-        return -1;
-        
+        size_offset += 2;
+        // handle rep
+        if(fnet_atoi(&pi->size, str + size_offset) == -1){
+            err_quit("in function get_packet_info: cannot parse packet string: %s", str);  
+        }
+       
     }
     
     sprintf(num_str, "%d\0", pi->size);
@@ -57,17 +63,19 @@ get_packet_size_dir_time(const char *str, struct packet_info * pi){
         pi->dir = 1;
     }
     else{
-        err_quit("%c string parser error", *(str + dir_offset));
+        err_quit("in function get_packet_info: cannot parse packet string: %s", str);  
     }
 
     time_offset = dir_offset + 9;
     if(fnet_atoi(&pi->ms_time, str + time_offset) == -1){
-        puts(str);
-        err_quit("error");
+        err_quit("in function get_packet_info: cannot parse packet string: %s", str);  
     }
     return 0;
 }
 
+/*
+ * parse packet feature with following format
+ */
 struct packets *
 parse_packets(const char * str_pkts){
     int offset = 0, num_pkt = 0, pkt_start, pkt_end, pkt_count = 0;
@@ -110,16 +118,13 @@ more:
     memcpy(pkt_str, str_pkts + pkt_start, pkt_end -pkt_start + 1);
     pkt_str[pkt_end -pkt_start + 1] = '\0';
 
-    get_packet_size_dir_time(pkt_str, pkts->packets + pkt_count);
+    get_packet_info(pkt_str, pkts->packets + pkt_count);
     pkt_count++;
     if(pkt_count == num_pkt){
         return pkts;
     }
     offset++;
     goto more;
-
-
-    
 }
 FILE * file;
 
@@ -132,7 +137,11 @@ int flow_count;
 
 char * data_file = "TWdata200_2";
 
-void handler(const unsigned char* arg, struct feature_set * fts){
+
+void 
+lab_task_data_traffic_handler(const unsigned char* arg, struct feature_set * fts){
+
+    // we regard the stream whose receiving time is less than 1s as the stream in the same pcap file 
     if(last_handler_time == -1 || time(NULL) - last_handler_time > 1){
         char file_path[256];
         last_handler_time = time(NULL);
@@ -153,6 +162,7 @@ void handler(const unsigned char* arg, struct feature_set * fts){
     }
     char start_timebuf[256], end_timebuf[256];
     time_t ms_start_flow, ms_end_flow;
+
     if(fts->f_feature[TIME_START] && fts->f_feature[TIME_END]){
         double start_time = atof(fts->features[TIME_START]->ft_val);
         double end_time = atof(fts->features[TIME_END]->ft_val);
@@ -164,9 +174,11 @@ void handler(const unsigned char* arg, struct feature_set * fts){
         strftime(end_timebuf, 256, "%Y-%m-%d_%H:%M:%S\0", localtime(&et));
         
         
+    } else{
+        return;
     }
     
-    
+    // parse packets feature
     struct packets * pkts = parse_packets(fts->features[PACKETS]->ft_val);
     if(pkts == NULL){
         err_msg("error");
@@ -176,12 +188,12 @@ void handler(const unsigned char* arg, struct feature_set * fts){
     for(struct action *a = action_list; a!= NULL; a=a->next){
         //printf("action:%s, start_time:%ld, end_time:%ld\n", a->action, a->start_time, a->end_time);
         int it = 0;
-        int vaild_f = 0;
+        int valid_f = 0;
         for(int i=0; i < pkts->no_pkt; i++){
             it += pkts->packets[i].ms_time;
             time_t pkt_time = ms_start_flow + it;
             if(pkt_time > a->start_time && pkt_time < a->end_time){
-                if(!vaild_f){
+                if(!valid_f){
                     fprintf(file, "%d,%s,\"[", flow_count, a->action);
                     if(pkts->packets[i].dir == 0){
                         fprintf(file, "%d" , pkts->packets[i].size);
@@ -189,7 +201,7 @@ void handler(const unsigned char* arg, struct feature_set * fts){
                     else{
                         fprintf(file, "-%d", pkts->packets[i].size);
                     }
-                    vaild_f = 1;
+                    valid_f = 1;
                     continue;
                 }
                 if(pkts->packets[i].dir == 0){
@@ -201,24 +213,14 @@ void handler(const unsigned char* arg, struct feature_set * fts){
                 
             }
         }
-        if(vaild_f){
+        if(valid_f){
             fprintf(file, "]\"");
             fprintf(file, ",%d.pcap", csv_file_count-1);
-            // test
-            //fprintf(file, ",%s,%s,", start_timebuf, end_timebuf);
-            //fprintf(file, "%l,%l,", a->start_time, a->end_time);
-
-            // end test
             fprintf(file, ",%s,%s,%s,%s,%s\n", fts->features[PR]->ft_val, 
                                     fts->features[SA]->ft_val,
                                     fts->features[DA]->ft_val,
                                     fts->features[SP]->ft_val,
                                     fts->features[DP]->ft_val);
-            
-            if(strcmp(fts->features[PR]->ft_val, "6")){
-                fflush(file);
-                getchar();
-            }
         }   
         
     }
@@ -227,10 +229,26 @@ void handler(const unsigned char* arg, struct feature_set * fts){
     fflush(file);
 }
 
+#define LOGFILE_MAX_LINE 256
+#define LOGFILE_START_STRING    "#start"
+#define LOGFILE_END_STRING  "#end"
+/*
+ * format of logfile
+ *  #start
+ * 
+ *  start time of action
+ *  action
+ *  end time of action
+ * 
+ *  [repeat]....
+ * 
+ *  #end
+ * 
+ */
 int
 read_logfile(char *file_path){
     FILE * file = NULL;
-    char line[256];
+    char line[LOGFILE_MAX_LINE];
     struct tm tm;
     int action_flag = 0, time_f = 0;
     struct action * cur_action;
@@ -240,14 +258,22 @@ read_logfile(char *file_path){
         err_quit("cannot open logfile");
     }
     do{
+        // handle illegal logfile
+        if(feof(file)){
+            if(action_flag){
+                err_quit("In function read_logfile: %s do not have string %s", file_path, LOGFILE_END_STRING);
+            }
+            else{
+                err_quit("In function read_logfile: %s do not have string %s", file_path, LOGFILE_START_STRING);
+            }
+        }
         fgets(line, 256, file);
         line[strlen(line)-1] = '\0';
-        if(strcmp(end_string, line) == 0){
+        if(strcmp(LOGFILE_END_STRING, line) == 0){
             break;
         }
         puts(line);
-        if(action_flag == 0 && strcmp(start_string, line) == 0){
-            
+        if(action_flag == 0 && strcmp(LOGFILE_START_STRING, line) == 0){
             action_flag = 1;
             time_f = 0;
             continue;
@@ -256,28 +282,30 @@ read_logfile(char *file_path){
         else if(action_flag == 0){
             continue;
         }
-        if(strlen(line) <= 1){
+        // empty line(no letter) may have a blank
+        if(strlen(line) <= 1){ 
             continue;
         }
+
         // read start time of action || read end time of action
         if(time_f == 0 || time_f == 2){
-            int _flag = 0, i=0;
-            time_t time, m_time;
-            do{
-                if(_flag != 1 && *(line+i) == '_'){
-                    *(line+i) = ' ';
-                    _flag = 1;
-                }
-                else if(_flag == 1 && *(line+i) == '_'){
-                    *(line+i) = '\0';
-                    m_time = atoi(line+i+1);
-                    puts(line);
-                    strptime(line, "%Y-%m-%d %H:%M:%S", &tm);
-                    printf("mktime:%ld\n", mktime(&tm));
-                    _flag = 2;
-                }
-                i++;
-            }while(_flag < 2);
+            time_t time;
+            int  m_time;
+            char * ms_str;
+
+            memset(&tm, 0, sizeof(struct tm)); // **no initialization will make the reture value of function mktime be -1**
+            ms_str = strptime(line, "%Y-%m-%d_%H:%M:%S", &tm);
+            if(ms_str == NULL){
+                err_quit("In function read_logfile: cannot parse time string %s", line);
+            }
+            if(fnet_atoi(&m_time, ms_str + 1) == -1){ // skip '_' character
+                err_quit("In function read_logfile: cannot parse time string %s", line);
+            }
+            // check return value of function mktime 
+            if((time = mktime(&tm)) == -1){
+                err_quit("In function read_logfile: value of mktime returns is -1");
+            }
+            printf("value of mktime returns:%ld\nmillsecond:%d\n", time, m_time);
             if(time_f == 0){
                 if(action_list == NULL){
                     cur_action = (struct action*)malloc(sizeof (struct action));
@@ -290,11 +318,11 @@ read_logfile(char *file_path){
                     cur_action->next = (struct action*)malloc(sizeof (struct action));
                     cur_action = cur_action->next;
                 }
-                cur_action->start_time = mktime(&tm) * 1000 + (int)(m_time / 1000000);
+                cur_action->start_time = time * 1000 + (int)(m_time / 1000000);
                 time_f = 1;
             }
             else{
-                cur_action->end_time = mktime(&tm) * 1000 + (int)(m_time / 1000000);
+                cur_action->end_time = time * 1000 + (int)(m_time / 1000000);
                 time_f = 0;
                 
             }
@@ -305,11 +333,12 @@ read_logfile(char *file_path){
             strcpy(cur_action->action, line);
             time_f = 2;
         }
-    }while(strcmp(end_string, line) != 0);
+    }while(1);
 }
 
+
 int 
-main(void){
+main(int argc, char * argv[]){
     if(init_connect_manage() < 0){
         err_quit("connection initialized failed");
     }
@@ -347,6 +376,6 @@ main(void){
     // // }
 
     // fprintf(file, "flow_number,action,packets_length_total\n");
-    dispatch(handler, NULL);
+    dispatch(lab_task_data_traffic_handler, NULL);
 }
 
